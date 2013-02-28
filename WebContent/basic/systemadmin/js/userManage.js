7dee6f4958b8a83ad23e952d50e0e82bce9243ce
firefoxmmx2 <firefoxmmx@gmail.com>
2013-02-28 17:23:59 +0800

    完善用户管理

--- WebContent/basic/systemadmin/js/userManage.js
+++ WebContent/basic/systemadmin/js/userManage.js
@@ -8,7 +8,8 @@
 Ext.ns('techsupport.systemmanage')
 
 if(!techsupport.systemmanage.UserManager){
-	techsupport.systemmanage.UserManager = Ext.extend(techsupport.systemmanage.SystemMain,{
+	let um;
+	um = techsupport.systemmanage.UserManager = Ext.extend(Ext.Panel,{
 		title_base : "用户",
 		layout:'border',
 		width:'100%',
@@ -17,7 +18,7 @@ if(!techsupport.systemmanage.UserManager){
 			forceFit:true
 		},
 		pagesize:25,
-		dir : "nodeorder",
+		dir : "userorder",
 		addURL : context_path+'/sysadminDefault/add_user.action',
 		modifyURL : context_path+'/sysadminDefault/modify_user.action',
 		queryURL : context_path+'/sysadminDefault/querylist_user.action',
@@ -25,56 +26,98 @@ if(!techsupport.systemmanage.UserManager){
 		removeURL : context_path+'/sysadminDefault/remove_user.action',
 		actionPrefix : 'user.',
 		removePrefix : 'userList[i].',
-		//构造器
-		constructor:function(config){
-			this.width = config.width || '100%';
-			this.height = config.width || '100%';
-			this.pagesize = config.pagesize || this.pagesize;//设置默认的每页显示记录数
-			this.dir = config.dir || 'nodeorder'; //设置默认的排序字段
-
-			techsupport.systemmanage.UserManager.superclass.apply(this,arguments);
-		},
-		//组件初始化
-		initComponent:function(ct,position){
-			var detail_panel_items_defaults = {
-				xtype:'textfield',
-				columnWidth: .25
-			}; 
-			this.detail_panel = Ext.create({
-				xtype:"form",
-				layout:'column',
-				viewConfig:{forceFit:true},
-				frame:false,
-				defaults:{
-					border:false,
-					bodyStyle:'padding:2',
-					labelAlign:'right'
-				},
-				items:[
-					{
-			    	   layout:'form',
-			    	   xtype:'panel',
-			    	   defaults:detail_panel_items_defaults,
-			    	   items:[
-			    	          {name:'username',fieldLabel:this.title_base+'名称'}
-			    	   ]
-					},
-					{
-						layout:'form',
-						xtype:'panel',
-						defaults:detail_panel_items_defaults,
-						items:[{name:'useraccount',fieldLabel:this.title_base+'帐号'}]
-					},
-					{
-						layout:'form',
-						xtype:'panel',
-						defaults:detail_panel_items_defaults,
-						items:[{name:'mobile',fieldLabel:this.title_base+'电话'}]
+		gridStore : Ext.create({
+			xtype:'jsonstore',
+			idProperty:'userid',
+			root:'userList',
+			url:um.url,
+			baseParams:{
+				start:0,
+				limit:um.pagesize,
+				dir:um.dir,
+				sort:um.sort
+			},
+			remoteSort:true,
+			totalProperty:'total',
+			fields[
+				{name:'userid',mapping:'userid'},
+				{name:'username',mapping:'username'},
+				{name:'useraccount',mapping:'useraccount'},
+				{name:'password',mapping:'password'},
+				{name:'departid',mapping:'departid'},
+				{name:'userorder',mapping:'userorder'},
+				{name:'isvalid',mapping:'isvalid'},
+				{name:'usertype',mapping:'usertype'},
+				{name:'idnum',mapping:'idnum'},
+				{name:'mobilephone',mapping:'mobilephone'},
+				{name:'email',mapping:'email'}
+			]
+		}),
+		treeLoader:Ext.create({
+			url:um.queryURL,
+			method:'post',
+			listeners:{
+				beforeload:{
+					fn:function(loader,node){
+						loader.baseParams[this.actionPrefix+'userid'] = node.id;
 					}
-				]
-			});
-			techsupport.systemmanage.UserManager.superclass.call(this);
-		}
+				}
+			}
+		},Ext.tree.TreeLoader),
+		treePanel : Ext.create({
+			xtype:'treepanel',
+			region:'west',
+			id:um.id+"TreePanel",
+			title:"机构树",
+			useArrows:true,
+			autoScroll:true,
+			animate:true,
+			enableDD:false,
+			containerScroll:true,
+			border:false,
+			width:'25%',
+			rootVisable:false,
+			viewConfig:{
+				forceFit:true,
+			},
+			loader:um.treeLoader,
+			root:{id:'0',text:'顶端',nodeType:'async'},
+			listeners:{
+				click:function(node,evt){
+					this.ownerCt.currentNodeId = node.id;
+					this.ownerCt.store.load({
+						params:{
+							start:0,
+							limit:this.ownerCt.pagesize,
+							dir:this.ownerCt.dir,
+							desc:this.ownerCt.desc
+						}
+					});
+				}
+			}
+		}),
+		gridSelectionModel:new Ext.grid.CheckboxSelectionModel(),
+		gridPanel:Ext.create({
+			xtype:'grid',
+			id:um.id+"Grid",
+			store:um.store,
+			border:false,
+			viewConfig:{
+				forceFit:true
+			},
+			sm:um.gridSelectionModel,
+			tbar:[
+				{xtype:'button',text:'添加',handler:function(){
+
+															  }},
+				{xtype:'button',text:'修改',handler:function(){}},
+				{xtype:'button',text:'删除',handler:function(){}},
+				{xtype:'button',text:'置顶',handler:function(){}},
+				{xtype:'button',text:'上移',handler:function(){}},
+				{xtype:'button',text:'下移',handler:function(){}},
+				{xtype:'button',text:'置底',handler:function(){}}
+			]
+		})
 	});
 
 }
