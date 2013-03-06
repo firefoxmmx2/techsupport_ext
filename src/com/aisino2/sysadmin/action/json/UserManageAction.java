d68232646f38fd383e63c3ef6149496d3c3c0122
firefoxmmx2 <firefoxmmx@gmail.com>
2013-03-06 17:28:07 +0800

    初始化用户管理视图

--- src/com/aisino2/sysadmin/action/json/UserManageAction.java
+++ src/com/aisino2/sysadmin/action/json/UserManageAction.java
@@ -1,9 +1,13 @@
 package com.aisino2.sysadmin.action.json;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.aisino2.sysadmin.action.PageAction;
+import com.aisino2.sysadmin.domain.User;
+import com.aisino2.sysadmin.service.IUserService;
 
 @Component
 @Scope("prototype")
@@ -13,9 +17,30 @@ public class UserManageAction extends PageAction {
 	 * 
 	 */
 	private static final long serialVersionUID = -8965936252677047815L;
+
 	
+	private User user;
+	private List<User> userList;
+	
+	private IUserService userService;
+	
+	public void setUserService(IUserService userService) {
+		this.userService = userService;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<User> getUserList() {
+		return userList;
+	}
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
 	/**
-	 * 用户列表查询
+	 * 用户单一查询
 	 * @return
 	 * @throws Exception
 	 */
@@ -28,7 +53,7 @@ public class UserManageAction extends PageAction {
 	 * @return
 	 * @throws Exception
 	 */
-	public String add_user() throws Exception{
+	public String add() throws Exception{
 		return SUCCESS;
 	}
 	
@@ -37,7 +62,7 @@ public class UserManageAction extends PageAction {
 	 * @return
 	 * @throws Exception
 	 */
-	public String mod_user() throws Exception{
+	public String modify() throws Exception{
 		return SUCCESS;
 	}
 	/**
@@ -45,7 +70,7 @@ public class UserManageAction extends PageAction {
 	 * @return
 	 * @throws Exception
 	 */
-	public String remove_user() throws Exception{
+	public String remove() throws Exception{
 		return SUCCESS;
 	}
 	/**
@@ -53,7 +78,16 @@ public class UserManageAction extends PageAction {
 	 * @return
 	 * @throws Exception
 	 */
-	public String check_user() throws Exception{
+	public String check() throws Exception{
+		return SUCCESS;
+	}
+	
+	/**
+	 * 用户列表查询
+	 * @return
+	 * @throws Exception
+	 */
+	public String querylist() throws Exception{
 		return SUCCESS;
 	}
 }
