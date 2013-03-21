114493272167959023cd77aa67b5461a20c3946c
firefoxmmx2 <firefoxmmx2>
2013-03-22 00:38:07 +0800

    添加字典相关的常量。

--- src/com/aisino2/sysadmin/Constants.java
+++ src/com/aisino2/sysadmin/Constants.java
@@ -1,5 +1,10 @@
 package com.aisino2.sysadmin;
 
+/**
+ * 系统常量
+ * @author hooxin
+ *
+ */
 public  abstract class Constants {
 	//session中存放用户的key
 	public static final String userKey="userKey_user";
@@ -19,4 +24,30 @@ public  abstract class Constants {
 	
 	//session中存放用户的key
 	public static final String csbm_Key="csidlixian";
+	
+	/**
+	 * 用户类别字典代码
+	 */
+	public static final String USER_TYPE_DICT_CODE = "dm_yhlb";
+	
+	/**
+	 * 技术支持单状态字典代码 
+	 */
+	public static final String ST_DM_TS_STATUS_DICT_CODE = "dm_ts_status";
+	
+	/**
+	 * 技术支持单地区角色字典代码
+	 */
+	public static final String ST_DM_TS_RM_MAP_DICT_CODE = "dm_ts_rm_map";
+	
+	/**
+	 * 技术支持单地区字典代码
+	 */
+	public static final String ST_DM_TS_REGIN_DICT_CODE = "dm_ts_regin";
+	
+	/**
+	 * 技术支持单流程环节字典代码
+	 */
+	public static final String ST_DM_TS_PROC_DICT_CODE = "dm_ts_proc";
+	
 }
