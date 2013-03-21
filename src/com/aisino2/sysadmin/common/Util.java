af7e305eac0ebf87243b5f30efebd3866d3baab0
firefoxmmx2 <firefoxmmx2>
2013-03-22 00:35:33 +0800

    添加注释信息。

--- src/com/aisino2/sysadmin/common/Util.java
+++ src/com/aisino2/sysadmin/common/Util.java
@@ -4,6 +4,11 @@ package com.aisino2.sysadmin.common;
  * 简单工具类
  */
 public class Util {
+	/**
+	 * 判断对象是否不为空
+	 * @param object 需要判断的对象
+	 * @return true为不为空，false为空
+	 */
 	public static boolean isNotEmpty(Object object){
 		if(object instanceof String){
 			if(object != null && ((String)object).length() > 0)
@@ -14,4 +19,5 @@ public class Util {
 				return true;
 		return false;  
 	}
+	
 }
